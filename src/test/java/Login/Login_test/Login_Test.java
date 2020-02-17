5a9b357a91807ceaaa2ff9314e48ecb2dd8dad6d
Admin <Admin@172.16.53.94>
2020-02-17 15:14:12 +0530

    Logout Details added

--- src/test/java/Login/Login_test/Login_Test.java
+++ src/test/java/Login/Login_test/Login_Test.java
@@ -12,6 +12,8 @@ public class Login_Test extends HelperClass{
 	public static void Login() {
 		
 		login("Helper","BaseURL", 1, 0, 1, 1);
+		explicitWaitForClickable(3000, "Helper", "Logout_Xpath");
+		clickWebelement("Helper", "Logout_Xpath");
 	
 	}
 public static void main(String[] args) {
